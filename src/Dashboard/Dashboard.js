49ae529ea23b49e66e80d0b1ea1ab5334cb33c8a
Mobina Abforoosh <mobinaabf@gmail.com>
2023-09-04 13:04:57 +0330

    create sidebar and impott icons

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,9 +1,81 @@
-import './Dashboard.css';
+import "./Dashboard.css";
+import { MdDashboard ,MdInventory ,MdAccountCircle} from "react-icons/md";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { AiFillProject } from "react-icons/ai";
+import { FaShoppingBasket, FaTasks } from "react-icons/fa";
 
 const Dashboard = () => {
   return (
     <div className="container">
-      <h2>hi</h2>
+      <div className="sidebar">
+        <div className="sidebar-brand">
+          <h1>
+            <span className="lab la-accusoft"> </span>
+            Data2Learn
+          </h1>
+        </div>
+
+        <div className="sidebar-menu">
+          <ul>
+            <li>
+              <a href="#">
+                <span className="material-icons-sharp">
+                  <MdDashboard />
+                </span>
+                <h3>Dashboard</h3>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="material-icons-sharp">
+                  <BsFillPeopleFill />
+                </span>
+                <h3>Customers</h3>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="material-icons-sharp">
+                  <AiFillProject />
+                </span>
+                <h3>Projects</h3>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="material-icons-sharp">
+                  <FaShoppingBasket />
+                </span>
+                <h3>Orders</h3>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="material-icons-sharp">
+                  <MdInventory />
+                </span>
+                <h3>Inventory</h3>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="material-icons-sharp">
+                  <MdAccountCircle />
+                </span>
+                <h3>Accounts</h3>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="material-icons-sharp">
+                  <FaTasks />
+                </span>
+                <h3>Tasks</h3>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
